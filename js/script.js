ec803a74b93dafa6d66476608eb61bbe061c57cb
Bakytzhan Kenzhebayev <bakhitzhankenzhebayev@gmail.com>
2023-05-31 04:34:52 +0600

    Урок 14 выполнен

--- js/script.js
+++ js/script.js
@@ -1,74 +1,116 @@
-const todoControl = document.querySelector('.todo-control'); // Получаем форму добавления дела
-const headerInput = document.querySelector('.header-input'); // Получаем поле ввода
-const todoList = document.querySelector('.todo-list'); // Получаем список дел
-const todoComplete = document.querySelector('.todo-completed'); // Получаем список выполненных дел
+const appData = {
+    todoControl: document.querySelector('.todo-control'),
+    headerInput: document.querySelector('.header-input'),
+    todoList: document.querySelector('.todo-list'),
+    todoComplete: document.querySelector('.todo-completed'),
+    toDoData: [],
 
-let toDoData = []; // Массив для хранения данных о делах
+    render: function () {
+        this.todoList.innerHTML = '';
+        this.todoComplete.innerHTML = '';
 
-const render = function () {
-    todoList.innerHTML = ''; // Очищаем список дел
-    todoComplete.innerHTML = ''; // Очищаем список выполненных дел
+        this.toDoData.forEach((item) => {
+            const li = document.createElement('li');
+            li.classList.add('todo-item');
 
-    // Цикл для отрисовки каждого дела из массива toDoData
-    toDoData.forEach(function (item) {
-        const li = document.createElement('li'); // Создаем элемент списка для дела
-        li.classList.add('todo-item'); // Добавляем класс к элементу списка
+            li.innerHTML = `<span class="text-todo">${item.text}</span>
+              <div class="todo-buttons">
+              <button class="todo-remove"></button>
+              <button class="todo-complete"></button>
+              </div>`;
 
-        // Формируем HTML-разметку для дела
-        li.innerHTML = '<span class="text-todo">' + item.text + '</span>' +
-            '<div class="todo-buttons">' +
-            '<button class="todo-remove"></button>' +
-            '<button class="todo-complete"></button>' +
-            '</div>';
+            if (item.completed) {
+                this.todoComplete.append(li);
+            } else {
+                this.todoList.append(li);
+            }
 
-        if (item.completed) {
-            todoComplete.append(li); // Если дело выполнено, добавляем его в список выполненных дел
-        } else {
-            todoList.append(li); // Иначе добавляем его в список дел
+            li.querySelector('.todo-complete').addEventListener('click', () => {
+                item.completed = !item.completed;
+                this.render();
+            });
+
+            li.querySelector('.todo-remove').addEventListener('click', () => {
+                this.toDoData = this.toDoData.filter((todo) => todo !== item);
+                this.render();
+            });
+        });
+    },
+
+    saveToLocalStorage: function () {
+        localStorage.setItem('toDoData', JSON.stringify(this.toDoData));
+    },
+
+    loadFromLocalStorage: function () {
+        const data = localStorage.getItem('toDoData');
+        if (data) {
+            this.toDoData = JSON.parse(data);
+            this.render();
+        }
+    },
+
+    handleFormSubmit: function (event) {
+        event.preventDefault();
+
+        const inputValue = this.headerInput.value.trim();
+        if (inputValue === '') {
+            return;
         }
 
-        li.querySelector('.todo-complete').addEventListener('click', function () {
-            item.completed = !item.completed; // Переключаем состояние выполнения дела
-            render(); // Обновляем отображение списка дел (рекурсивный вызов функции render)
+        const newToDo = {
+            text: inputValue,
+            completed: false,
+        };
+
+        this.toDoData.push(newToDo);
+        this.headerInput.value = '';
+        this.saveToLocalStorage();
+        this.render();
+    },
+
+    reset: function () {
+        this.todoControl.removeEventListener('submit', this.handleFormSubmit);
+        this.todoControl.addEventListener('submit', this.handleFormSubmit.bind(this));
+
+        this.todoList.innerHTML = '';
+        this.todoComplete.innerHTML = '';
+        this.headerInput.value = '';
+
+        const resetButton = document.getElementById('reset');
+        resetButton.replaceWith(this.calculateButton);
+        this.calculateButton.addEventListener('click', this.reset.bind(this));
+
+        const inputs = document.querySelectorAll('input[type=text], select');
+        inputs.forEach((input) => {
+            input.disabled = false;
         });
 
-        li.querySelector('.todo-remove').addEventListener('click', function () {
-            toDoData = toDoData.filter((todo) => todo !== item); // Удаляем дело из массива
-            render(); // Обновляем отображение списка дел (рекурсивный вызов функции render)
+        this.toDoData = [];
+        this.saveToLocalStorage();
+    },
+
+    start: function () {
+        this.loadFromLocalStorage();
+        this.todoControl.addEventListener('submit', this.handleFormSubmit.bind(this));
+
+        const calculateButton = document.getElementById('calculate');
+        this.calculateButton = calculateButton;
+        calculateButton.addEventListener('click', () => {
+            const inputs = document.querySelectorAll('input[type=text], select');
+            inputs.forEach((input) => {
+                input.disabled = true;
+            });
+
+            calculateButton.style.display = 'none';
+
+            const resetButton = document.createElement('button');
+            resetButton.id = 'reset';
+            resetButton.textContent = 'Reset';
+            this.resetButton = resetButton;
+            this.todoControl.append(resetButton);
+            resetButton.addEventListener('click', this.reset.bind(this));
         });
-    });
+    },
 };
 
-const saveToLocalStorage = function () {
-    localStorage.setItem('toDoData', JSON.stringify(toDoData)); // Сохраняем массив дел в localStorage
-};
-
-const loadFromLocalStorage = function () {
-    const data = localStorage.getItem('toDoData'); // Получаем данные из localStorage
-    if (data) {
-        toDoData = JSON.parse(data); // Если данные существуют, конвертируем их в массив дел
-        render(); // Обновляем отображение списка дел
-    }
-};
-
-// Обработчик события отправки формы
-todoControl.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    const inputValue = headerInput.value.trim();
-    if (inputValue === '') {
-        return; // Если поле ввода пустое, прекращаем выполнение функции
-    }
-
-    const newToDo = {
-        text: inputValue,
-        completed: false
-    };
-
-    toDoData.push(newToDo); // Добавляем новое дело в массив
-    headerInput.value = ''; // Очищаем поле ввода
-    saveToLocalStorage(); // Сохраняем данные в localStorage
-    render(); // Обновляем отображение списка дел
-});
-
-loadFromLocalStorage(); // Загружаем данные из localStorage при загрузке страницы
+appData.start();
